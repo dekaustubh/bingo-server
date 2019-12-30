5864071d29217b9180254ba870fc23149e090387
Kaustubh <kaustubh.deshmukh27@gmail.com>
2019-12-30 13:26:42 +0530

    Move away from H2 DB & migrate to Postgres

--- src/db/DatabaseFactory.kt
+++ src/db/DatabaseFactory.kt
@@ -1,8 +1,6 @@
 package com.dekaustubh.db
 
 import com.dekaustubh.models.Users
-import com.zaxxer.hikari.HikariConfig
-import com.zaxxer.hikari.HikariDataSource
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils.create
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
@@ -10,21 +8,15 @@ import org.jetbrains.exposed.sql.transactions.transaction
 
 object DatabaseFactory {
     fun init() {
-        Database.connect(hikari())
+        Database.connect(
+            driver = "org.postgresql.Driver",
+            url = "jdbc:postgresql://localhost:5432/postgres",
+            user = "postgres",
+            password = "kaustubh"
+        )
         transaction {
             create(Users)
         }
-    }
-
-    private fun hikari(): HikariDataSource {
-        val config = HikariConfig()
-        config.driverClassName = "org.h2.Driver"
-        config.jdbcUrl = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1"
-        config.maximumPoolSize = 10
-        config.isAutoCommit = false
-        config.transactionIsolation = "TRANSACTION_REPEATABLE_READ"
-        config.validate()
-        return HikariDataSource(config)
     }
 
     suspend fun <T> dbQuery(
