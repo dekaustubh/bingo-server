4cf165466a6f26ed58e41bc6b936fe79512a62aa
Kaustubh <kaustubh.deshmukh27@gmail.com>
2019-12-30 09:51:42 +0530

    Initial Commit

---
+++ src/Application.kt
@@ -0,0 +1,73 @@
+package com.dekaustubh
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.content.*
+import io.ktor.http.content.*
+import io.ktor.features.*
+import io.ktor.websocket.*
+import io.ktor.http.cio.websocket.*
+import java.time.*
+import com.fasterxml.jackson.databind.*
+import io.ktor.jackson.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(io.ktor.websocket.WebSockets) {
+        pingPeriod = Duration.ofSeconds(15)
+        timeout = Duration.ofSeconds(15)
+        maxFrameSize = Long.MAX_VALUE
+        masking = false
+    }
+
+    install(ContentNegotiation) {
+        jackson {
+            enable(SerializationFeature.INDENT_OUTPUT)
+        }
+    }
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+        // Static feature. Try to access `/static/ktor_logo.svg`
+        static("/static") {
+            resources("static")
+        }
+
+        install(StatusPages) {
+            exception<AuthenticationException> { cause ->
+                call.respond(HttpStatusCode.Unauthorized)
+            }
+            exception<AuthorizationException> { cause ->
+                call.respond(HttpStatusCode.Forbidden)
+            }
+
+        }
+
+        webSocket("/myws/echo") {
+            send(Frame.Text("Hi from server"))
+            while (true) {
+                val frame = incoming.receive()
+                if (frame is Frame.Text) {
+                    send(Frame.Text("Client said: " + frame.readText()))
+                }
+            }
+        }
+
+        get("/json/jackson") {
+            call.respond(mapOf("hello" to "world"))
+        }
+    }
+}
+
+class AuthenticationException : RuntimeException()
+class AuthorizationException : RuntimeException()
+
